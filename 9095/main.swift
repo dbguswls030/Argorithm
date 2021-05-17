857953c4731e5f9849d6ece5446bf66af5d5692e
dbguswls030 <dbguswls030@gmail.com>
2021-05-17 20:40:59 +0900

    finish

--- 9095/main.swift
+++ 9095/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ N:Int) -> Int{
+    if(N == 1) {
+        return 1
+    }
+    else if(N == 2) {
+        return 2
+    }
+    else if(N == 3) {
+        return 4
+    }
+    else {
+        return solution(N-3) + solution(N-2) + solution(N-1)
+    }
+}
+
+var c: Int = Int(readLine()!)!
+
+for _ in 0..<c{
+    var N: Int = Int(readLine()!)!
+
+    print(solution(N))
+
+}
+
+
 
