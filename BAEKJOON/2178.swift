7dd421f0925b1b69c519ee73149a1f60c29c7409
U <69714292+dbguswls030@users.noreply.github.com>
2021-09-28 16:53:24 +0900

    Update 2178.swift

--- BAEKJOON/2178.swift
+++ BAEKJOON/2178.swift
@@ -1,66 +1,55 @@
-//맵 입력 받기
-//주변 경로 탐색->다음 경로 스택에 넣기
-//경로 지날 때마다 count + 1
-//N,M 좌표일 때의 count를 저장
-//count 제일 적은 경우를 출력
+
 import Foundation
-
-
-func searchPath(_ x: Int, _ y: Int, _ count: Int ){
-    map[x][y] = 0
-    if x+1 <= input[0]-1{
-        if map[x+1][y] == 1{ //하
-            stack.append([x+1, y, count+1])
-        }
-    }
-    if y-1 >= 0{
-        if map[x][y-1] == 1{ //좌
-            stack.append([x, y-1, count+1])
-        }
-    }
-    if x-1 >= 0{
-        if map[x-1][y] == 1{ //상
-            stack.append([x-1, y, count+1])
-        }
-    }
+func solution(){
+    let input = readLine()!.components(separatedBy: " ").map() {Int($0)!}
+    var map = [[Int]]()
+    var queue = [[0,0]]
     
-    if y+1 <= input[1]-1{
-        if map[x][y+1] == 1{ //우
-            stack.append([x, y+1, count+1])
-        }
+
+    for _ in 0..<input[0]{
+        map.append(readLine()!.map({Int(String($0))!}))
     }
+
     
+    while queue.count != 0{
+        let v = queue.removeFirst()
+        let x = v[0]
+        let y = v[1]
+        
+        if x == input[0]-1, y == input[1]-1{
+            print(map[x][y])
+            return
+        }
+        
+        if x+1 <= input[0]-1{
+            if map[x+1][y] == 1{ //하
+                map[x+1][y] = map[x][y] + 1
+                queue.append([x+1, y])
+            }
+        }
+        if y-1 >= 0{
+            if map[x][y-1] == 1{ //좌
+                map[x][y-1] = map[x][y] + 1
+                queue.append([x, y-1])
+            }
+        }
+        if x-1 >= 0{
+            if map[x-1][y] == 1{ //상
+                map[x-1][y] = map[x][y] + 1
+                queue.append([x-1, y])
+            }
+        }
+        
+        if y+1 <= input[1]-1{
+            if map[x][y+1] == 1{ //우
+                map[x][y+1] = map[x][y] + 1
+                queue.append([x, y+1])
+            }
+        }
+        
+        
+        
+    }
 }
 
-func solution(_ x: Int, _ y: Int, _ count: Int) {
-    
-    searchPath(x, y, count)
-    //print("x: \(y), y: \(x), count: \(count)")
-    
-    
-    if x == input[0]-1, y == input[1]-1{
-        if result > count {
-            result = count
-        }
-    }
-    //print(stack)
-    if stack.count != 0{
-        let cur = stack.removeFirst()
-        solution(cur[0], cur[1], cur[2])
-    }
-    
-}
-
-var input = readLine()!.components(separatedBy: " ").map() {Int($0)!}
-var map = [[Int]]()
-var stack = [[0, 0, 1]]
-var result = 9999
-
-for _ in 0..<input[0]{
-    map.append(readLine()!.map({Int(String($0))!}))
-}
-
-
-let cur = stack.removeFirst() 
-solution(cur[0], cur[1], cur[2])
-print(result)
+solution()
