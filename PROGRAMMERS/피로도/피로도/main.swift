fe860edc7f95bb880b37820f17dee97fab9078f7
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2023-01-11 17:39:05 +0900

    Add files via upload

--- PROGRAMMERS/피로도/피로도/main.swift
+++ PROGRAMMERS/피로도/피로도/main.swift
@@ -10,17 +10,14 @@ import Foundation
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
     var count = 0
     func dfs(k: Int, dungeons: [[Int]], dept: Int){
-        
+        if dept > count { count = dept}
         for dungeon in dungeons {
             if k >= dungeon[0] {
                 dfs(k: k - dungeon[1], dungeons: dungeons.filter({$0 != dungeon}), dept: dept+1)
             }
         }
-        
     }
     dfs(k: k, dungeons: dungeons, dept: 0)
     return count
 }
-
-
 print(solution(80, [[80,20],[50,40],[30,10]]))
