60c06936554e46d88e48bf70ea00dc4fafd2aba8
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2023-01-13 13:59:09 +0900

    Add files via upload

--- PROGRAMMERS/나머지가1이 되는 수 찾기/나머지가1이 되는 수 찾기/main.swift
+++ PROGRAMMERS/나머지가1이 되는 수 찾기/나머지가1이 되는 수 찾기/main.swift
@@ -8,9 +8,10 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    
-    return 0
+    for i in 2..<n/2{
+        if n % i == 1 {return i }
+    }
+    return n-1
 }
-
 print(solution(10))
 print(solution(12))
