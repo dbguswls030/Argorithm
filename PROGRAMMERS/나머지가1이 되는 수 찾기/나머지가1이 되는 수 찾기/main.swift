706ce8a5ecfd5fcca0504455133e3db156318386
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2023-01-13 13:58:23 +0900

    Add files via upload

---
+++ PROGRAMMERS/나머지가1이 되는 수 찾기/나머지가1이 되는 수 찾기/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  나머지가1이 되는 수 찾기
+//
+//  Created by 유현진 on 2023/01/13.
+//
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    return 0
+}
+
+print(solution(10))
+print(solution(12))
