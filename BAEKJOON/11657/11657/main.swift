17e86a7d6727db190d556a944891a276f350762f
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2024-12-10 15:38:01 +0900

    Add files via upload

--- BAEKJOON/11657/11657/main.swift
+++ BAEKJOON/11657/11657/main.swift
@@ -11,45 +11,53 @@ func solution(){
     var input = readLine()!.split(separator: " ").map{Int(String($0))!}
     let N = input[0]
     let M = input[1]
-    var edges = Array(repeating: [(Int,Int)](), count: N+1)
-    var result = Array(repeating: Int.max, count: N+1)
+    var edges = Array(repeating: [(Int,Int)](), count: N)
+    var result = Array(repeating: Int.max, count: N)
     
     for _ in 0..<M{
         input = readLine()!.split(separator: " ").map{Int(String($0))!}
-        edges[input[0]].append((input[1], input[2]))
+        edges[input[0]-1].append((input[1]-1, input[2]))
     }
-    func bf(start: Int) -> Bool{
-        result[start] = 0
-        
-        for index in 1...N{
-            for cur in 1...N{
-                for edge in edges[cur]{
-                    let next = edge.0
-                    let cost = edge.1
-                    
-                    if result[cur] != Int.max, result[next] > result[cur] + cost{
-                        result[next] = result[cur] + cost
-                        
-                        if index == N{
-                            return true
-                        }
-                    }
+    
+    result[0] = 0 // 시작 노드는 0으로 초기화
+    for i in 0..<N{ // N-1번 반복
+        for cur in 0..<edges.count{
+            for edge in edges[cur]{
+                let next = edge.0
+                let cost = edge.1
+                
+                if result[cur] != Int.max, result[next] > result[cur] + cost{
+                    result[next] = result[cur] + cost
                 }
             }
         }
-        return false
     }
     
-    if bf(start: 1){
-        print("-1")
-    }else{
-        for i in 2...N{
-            if result[i] == Int.max{
+    for cur in 0..<edges.count{
+        for edge in edges[cur]{
+            let next = edge.0
+            let cost = edge.1
+            
+            if result[cur] != Int.max, result[next] > result[cur] + cost{
                 print("-1")
-            }else{
-                print(result[i])
+                return
             }
+        }
+    }
+    
+    for i in 1..<N{
+        if result[i] == Int.max{
+            print("-1")
+        }else{
+            print(result[i])
         }
     }
 }
 solution()
+//4 6
+//1 2 3
+//1 3 2
+//1 4 5
+//2 3 3
+//3 4 -4
+//4 2 -4
