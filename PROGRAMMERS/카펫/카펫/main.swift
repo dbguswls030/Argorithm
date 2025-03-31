0ba480584942000eeb781263a7be96c06ed98b49
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2025-03-31 13:53:30 +0900

    Add files via upload

--- PROGRAMMERS/카펫/카펫/main.swift
+++ PROGRAMMERS/카펫/카펫/main.swift
@@ -2,7 +2,7 @@
 //  main.swift
 //  카펫
 //
-//  Created by 유현진 on 2023/01/13.
+//  Created by 유현진 on 3/31/25.
 //
 
 import Foundation
@@ -10,25 +10,32 @@ import Foundation
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
     let sum = brown + yellow
     var com = [[Int]]()
-    for i in 3...sum/3{
-        if sum % i == 0 {
-            let w = sum / i
-            let h = i
-            if h>w{break}
-            com.append([w,h])
+    var result = [Int]()
+    
+    // 최소 가로 길이 3부터
+    for h in 3...sum/3{
+        if sum % h == 0 {
+            let width = sum / h
+            let height = h
+            
+            if height > width{ break } // 가로가 높이와 같거나 더 길어야 함
+            
+            com.append([width,height])
         }
     }
+    
     for i in com{
         let w = i[0]
         let h = i[1]
         if (w-2)*(h-2) == yellow{
-            return [w,h]
+            result = [w, h]
+            break
         }
     }
     
-    return []
+    return result
 }
+
 print(solution(10, 2))
 print(solution(8, 1))
 print(solution(24, 24))
-
