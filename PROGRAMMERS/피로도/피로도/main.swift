86eaf8a73cf708e3c253bde37e4299bdd0aa0891
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2025-04-02 09:39:37 +0900

    Add files via upload

--- PROGRAMMERS/피로도/피로도/main.swift
+++ PROGRAMMERS/피로도/피로도/main.swift
@@ -2,22 +2,29 @@
 //  main.swift
 //  피로도
 //
-//  Created by 유현진 on 2023/01/11.
+//  Created by 유현진 on 4/2/25.
 //
 
 import Foundation
 
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
     var count = 0
-    func dfs(k: Int, dungeons: [[Int]], dept: Int){
-        if dept > count { count = dept}
-        for dungeon in dungeons {
-            if k >= dungeon[0] {
-                dfs(k: k - dungeon[1], dungeons: dungeons.filter({$0 != dungeon}), dept: dept+1)
+    
+    func dfs(k: Int,  visited: [Bool], dept: Int){
+        var visited = visited
+        if count < dept{ count = dept }
+        
+        for (offset, dungeon) in dungeons.enumerated(){
+            if visited[offset] == false{
+                if k >= dungeon[0]{
+                    visited[offset] = true
+                    dfs(k: k-dungeon[1], visited: visited, dept: dept+1)
+                    visited[offset] = false
+                }
             }
         }
     }
-    dfs(k: k, dungeons: dungeons, dept: 0)
+    
+    dfs(k: k, visited: Array(repeating: false, count: dungeons.count), dept: 0)
     return count
 }
-print(solution(80, [[80,20],[50,40],[30,10]]))
